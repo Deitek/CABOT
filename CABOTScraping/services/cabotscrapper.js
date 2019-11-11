465372bfc9f8cf3cd7bf3f77df76142368617c40
erika.gutierrez <erika.gutierrez@etn.com.co>
2019-11-10 22:26:47 -0500

    Código limpio y ajustado para funcionar correctamente

--- CABOTScraping/services/cabotscrapper.js
+++ CABOTScraping/services/cabotscrapper.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const request = require('request');
 const fetch = require("node-fetch");
+const { SimilarSearch } = require('node-nlp');
+const similar = new SimilarSearch();
 
 
 var scrapePage = function(url, question) {
 
-    //window.location.hostname;//window.location.href;//window.location.hostname;
     const pageURL = url;
 
     //make an HTTP request for the page to be scraped
-    request(pageURL, function(error, response, responseHtml){
+    var response = request(pageURL, function(error, response, responseHtml){
+        var result = null;
         if (!error){
             //write the entire scraped page to the local file system
             let path = './HTML/entire-page.html';
             fs.writeFile(path, responseHtml, function(err){
-                console.log(err);
                 if(err === null){
                     let $ = cheerio.load(responseHtml);
                     //console.log($);
@@ -35,10 +36,11 @@ var scrapePage = function(url, question) {
                     //write the Text to the local file system
                     fs.writeFile('./HTML/data-clear.txt', t, function (err) {
                         if(err===null){
+
                             var json = {
                                 "utterances":[
                                     {
-                                        "link":"101",
+                                        "link": url,
                                         "searchSentence":question,
                                         "inputDoc":t,
                                         "prob": null
@@ -46,48 +48,23 @@ var scrapePage = function(url, question) {
                                 ]
                             }
 
-                            let response = fetch("http://127.0.0.1:5000/textsimilarity/v1", {
-                                method: 'POST',
-                                headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify({data: json})
-                            });
-
-                            response.then((x)=> console.log(x)) 
+                            console.log(t.substring(0,1000), "...", t.length);
+                            result = similar.getBestSubstring(t, question);
+                            console.log(result);
                         }
                     });
-
                 }
+
+                return result;
             })
-
-            //write isolated sections of the entire scraped page to the local file system
-
-            //create the cheerio object
-            /*const $ = cheerio.load(responseHtml),
-                //create a reference to the header element
-                $header = $('header').html(),
-                $content = $('#mainContent').html(),
-                $footer = $('footer').html();
-
-                
-
-            //write the header to the local file system
-            fs.writeFile(__dirname + '/HTML/header.html', $header, function(err){
-                console.log('header.html successfully written to HTML folder');
-            });
-
-            //write the footer to the local file system
-            fs.writeFile(__dirname + '/HTML/footer.html', $footer, function(err){
-                console.log('footer.html successfully written to HTML folder');
-            });*/
         }
         else if (error) {
             //Return Error
             if (error) return console.error(error);
         }
     });
+
+    return response;
 }
 
 
